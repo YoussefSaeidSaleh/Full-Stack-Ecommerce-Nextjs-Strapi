ca51448af761e3f4b7b1615e41504b09b49e33ec
Youssef Saeid <146128402+YoussefSaeidSaleh@users.noreply.github.com>
2026-03-10 13:15:49 +0200

    Fix API URL to use Strapi Cloud

--- app/_utils/axiosClient.js
+++ app/_utils/axiosClient.js
@@ -1,7 +1,11 @@
 const { default: axios } = require("axios");
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = "http://localhost:1337/api";
+
+// التعديل هنا: نستخدم المتغير البيئي ونضيف له /api
+// لاحظ أننا نستخدم BACKTICKS (`) وليس علامات تنصيص عادية
+const apiUrl = `${process.env.NEXT_PUBLIC_STRAPI_URL}/api`;
+// const apiUrl = "http://localhost:1337/api";
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
